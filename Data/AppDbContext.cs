7322a0ad42fe2af0abb808306d5c7396f601aa7f
BrunoFelipeMartins <bruno.f.martins1996@gmail.com>
2022-08-17 00:57:00 -0300

    coDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,7 +1,16 @@
+using MeuTodo.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace MeuTodo.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public DbSet<Todo> Todos { get; set; }
 
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+            =>optionsBuilder.UseSqlite(connectionString: "DataSource=app.db;Cache=Shared");
+
+        
     }
 }
