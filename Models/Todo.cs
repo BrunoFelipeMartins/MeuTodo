aedb22f5b3a2d50caf889850ef3bbf1158cc4a9c
BrunoFelipeMartins <bruno.f.martins1996@gmail.com>
2022-08-17 01:53:46 -0300

    TodoGet

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace MeuTodo.Models
 {
     public class Todo
@@ -5,5 +7,6 @@ namespace MeuTodo.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public bool Done { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
     }
 }
